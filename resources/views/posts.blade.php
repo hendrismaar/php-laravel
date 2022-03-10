108d72091450e062a5b50c87dd6478e18c012e79
Opilane TTHK <opilane@tthk.ee>
2022-03-10 13:07:21 +0200

    sql and blade stuff

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,20 +1,12 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="/css/minu.css">
-    <title>Blog posts</title>
-</head>
-<body>
+<x-layout>
     <?php foreach ($posts as $post) : ?>
         <article>
-            <a href="/posts/<?= $post->slug; ?>"><h1><?= $post->title; ?></h1></a>
+            <a href="/posts/{{$post->slug}}">
+                {{$post->title}}
+            </a>
             <div>
-                <?= $post->body; ?>
+                {{$post->excerpt}}
             </div>
         </article>
     <?php endforeach; ?>
-</body>
-</html>
+</x-layout>
